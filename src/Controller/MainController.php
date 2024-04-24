f289231f76a3ccbd7de4f167969f7876dc3c656d
Stefan <stefan@studiokhi.com>
2024-04-24 17:00:59 +0200

    feat: faq, studio, projects, show, blog, post

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -18,8 +18,42 @@ class MainController extends AbstractController
     #[Route('/service/{slug}', name: 'service')]
     public function service(): Response
     {
-
-
         return $this->render('main/service.html.twig');
+    }
+
+    #[Route('/studio', name: 'studio')]
+    public function studio(): Response
+    {
+        return $this->render('main/studio.html.twig');
+    }
+
+    #[Route('/realisations', name: 'projects')]
+    public function projects(): Response
+    {
+        return $this->render('main/projects.html.twig');
+    }
+
+    #[Route('/realisations/{slug}', name: 'show')]
+    public function show(): Response
+    {
+        return $this->render('main/show.html.twig');
+    }
+
+    #[Route('/blog', name: 'posts')]
+    public function posts(): Response
+    {
+        return $this->render('main/posts.html.twig');
+    }
+
+    #[Route('/blog/{slug}', name: 'post')]
+    public function post(): Response
+    {
+        return $this->render('main/post.html.twig');
+    }
+
+    #[Route('/faq', name: 'faq')]
+    public function faq(): Response
+    {
+        return $this->render('main/faq.html.twig');
     }
 }
