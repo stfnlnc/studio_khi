0c155d8f8ad42a4cf6c2d2bba8018c093cf0fb65
Stefan <stefan@studiokhi.com>
2024-07-30 17:51:12 +0200

    feat: gsap animations

--- assets/app.js
+++ assets/app.js
@@ -1,33 +1,225 @@
 import './styles/app.css';
-import './react/Component.jsx'
-import './react/Cookies.jsx'
-import './react/Faq.jsx'
-import './react/Tag.jsx'
-import './react/Filter.jsx'
-import './react/Dropdown.jsx'
-import './react/DropdownMobile.jsx'
-import './react/MenuItem.jsx'
-import './react/MenuIcon.jsx'
-import './react/Link.jsx'
-import './react/Button.jsx'
+
 import 'htmx.org';
+import { gsap } from "gsap-trial";
+
+/* The following plugins are Club GSAP perks */
+import { ScrollSmoother } from "gsap-trial/ScrollSmoother";
+import { MorphSVGPlugin } from "gsap-trial/MorphSVGPlugin";
+import { SplitText } from "gsap-trial/SplitText";
+import { ScrollTrigger } from "gsap-trial/ScrollTrigger";
+import { Draggable } from "gsap-trial/Draggable";
+
+
+gsap.registerPlugin(ScrollSmoother,MorphSVGPlugin,SplitText, ScrollTrigger, Draggable);
 
 window.htmx = require('htmx.org');
 
 htmx.onLoad(function () {
 
-// Preloader
     const preloader = document.querySelector('.preloader')
     const preloaderIcon = document.querySelector('.preloader__icon')
     const preloaderText = document.querySelector('.preloader__text')
 
-    if (preloader) {
-        preloaderIcon.classList.add('preloader__icon-active')
-        preloaderText.classList.add('preloader__text-active')
-        setTimeout(() => {
-            preloader.classList.add('preloader__remove')
-        }, 1000)
+    gsap.to(preloaderIcon, {
+        x: 0,
+        y: 0,
+        duration: 0.8,
+        opacity: 1,
+        ease: 'power2.out'
+    })
+    gsap.to(preloaderText, {
+        y: 0,
+        duration: 0.8,
+        opacity: 1,
+        ease: 'power2.out'
+    })
+    gsap.to(preloader, {
+            y: '-100%',
+            delay: 0.6,
+            duration: 1.3,
+            ease: "power2.inOut"
+    })
+
+    const topContent = document.querySelector('#top-content')
+
+    gsap.to(topContent, {
+        filter: 'blur(8px)',
+        duration: 1,
+        scrollTrigger: {
+            pin: true,
+            trigger: topContent,
+            start: "top top",
+            scrub: 1,
+            markers: false
+        }
+    })
+
+    const branding = document.querySelector('#branding')
+    const webdesign = document.querySelector('#webdesign')
+    const dev = document.querySelector('#dev')
+
+    const tl = gsap.timeline()
+
+    tl.to(branding, {
+        filter: 'blur(8px)',
+        duration: 1,
+        scrollTrigger: {
+            pin: true,
+            trigger: branding,
+            start: "0% top",
+            scrub: 1,
+            markers: false
+        }
+    }).to(webdesign, {
+        duration: 2,
+        scrollTrigger: {
+            trigger: branding,
+            start: "bottom bottom",
+            end: 'top top',
+            scrub: 1,
+            markers: false
+        }
+    }).to(webdesign, {
+        filter: 'blur(8px)',
+        duration: 1,
+        scrollTrigger: {
+            pin: true,
+            trigger: webdesign,
+            start: "top top",
+            end: '100% top',
+            scrub: 1,
+            markers: false
+        }
+    }).to(dev, {
+        duration: 2,
+        scrollTrigger: {
+            trigger: webdesign,
+            start: "bottom bottom",
+            end: 'top top',
+            scrub: 1,
+            markers: false
+        }
+    }).to(dev, {
+        filter: 'blur(8px)',
+        duration: 1,
+        scrollTrigger: {
+            pin: true,
+            trigger: dev,
+            start: "top top",
+            end: '100% top',
+            scrub: 1,
+            markers: false
+        }
+    })
+
+    const reveals = document.querySelectorAll(".reveal");
+    if (reveals) {
+        reveals.forEach((reveal) => {
+            gsap.from(reveal, {
+                y: 200,
+                opacity: 0,
+                duration: 1,
+                delay: 1.2,
+                ease: "power4"
+            });
+        })
     }
+
+    const lines = document.querySelectorAll('.line')
+
+    lines.forEach(line => {
+        gsap.from(line, {
+            scaleX: 0,
+            duration: 1,
+            transformOrigin: "left center",
+            ease: "power2.inOut",
+            scrollTrigger: {
+                trigger: line,
+                start: "top 95%",
+                toggleActions: "play none none none",
+                markers: false
+            }
+        });
+    });
+
+    const titles = document.querySelectorAll(".title");
+    if (titles) {
+        titles.forEach((title) => {
+            const splitTitle = new SplitText(title, {type: "words"});
+            const chars = splitTitle.words;
+
+            gsap.from(chars, {
+                scrollTrigger: {
+                    trigger: title,
+                    start: 'top 80%',
+                    end: '',
+                    markers: false
+                },
+                y: '100%',
+                opacity: 0,
+                duration: 0.5,
+                ease: 'power2.out',
+                stagger: 0.1,
+            });
+        })
+    }
+
+    const headings = document.querySelectorAll(".heading");
+    if (headings) {
+        headings.forEach((heading) => {
+            const splitHeading = new SplitText(heading, {type: "words,chars"});
+            const chars = splitHeading.chars;
+
+            gsap.from(chars, {
+                scrollTrigger: {
+                    trigger: heading,
+                    start: 'top 90%',
+                    end: '',
+                    markers: false
+                },
+                x: '50%',
+                opacity: 0,
+                duration: 0.08,
+                ease: 'power2.out',
+                stagger: 0.05,
+            });
+        })
+    }
+
+    const faqsTitle = document.querySelectorAll('.faq__title')
+    const faqsAnswer = document.querySelectorAll('.faq__answer')
+    const faqsIcon = document.querySelectorAll('.faq__icon')
+
+    faqsTitle.forEach((title, key) => {
+        title.addEventListener('click', () => {
+            let maxHeight
+            let rotate
+            if(faqsAnswer[key].style.maxHeight === '400px') {
+                maxHeight = 0
+                rotate = 0
+            } else {
+                maxHeight = 400
+                rotate = 45
+            }
+            gsap.to(faqsAnswer[key], {
+                maxHeight: maxHeight,
+                duration: 0.8,
+                ease: 'power2.inOut'
+            })
+            gsap.to(faqsIcon[key], {
+                rotation: rotate,
+                duration: 0.1,
+                ease: 'power2.inOut'
+            })
+        })
+    })
+
+
+    ScrollSmoother.create({
+        smooth: 2,
+        effects: true
+    });
 
 
 // Resize header when scrolling
