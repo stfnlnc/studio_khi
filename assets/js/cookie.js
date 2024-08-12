05251c6ebe5c083c916273b581161ba6f7e23f2c
Stefan <stefan@studiokhi.com>
2024-08-12 15:52:41 +0200

    feat: calendly link

--- assets/js/cookie.js
+++ assets/js/cookie.js
@@ -1,6 +1,13 @@
 export function cookie() {
     const cookies = document.getElementById('cookies')
+    const cookieAgree = document.querySelector('.agree')
     if (cookies) {
+        cookieAgree.addEventListener('click', () => {
+            document.cookie = 'cookies=agree';
+            cookies.style.display = 'none'
+        })
+
+
         if (document.cookie.includes('cookies=agree')) {
             cookies.style.display = 'none'
         }
---
+++ migrations/Version20240812132634.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20240812132634 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE informations (id INT AUTO_INCREMENT NOT NULL, phone VARCHAR(255) DEFAULT NULL, email VARCHAR(255) DEFAULT NULL, copyright VARCHAR(255) DEFAULT NULL, calendly VARCHAR(255) DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE informations');
+    }
+}
